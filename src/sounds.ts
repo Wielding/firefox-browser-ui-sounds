976c0317c47019a777578b9919c7abe361c974f6
Andrew <andrew@wielding.net>
2024-06-21 09:42:55 -0400

    switched to mp3 files since wav files stopped playing

--- src/sounds.ts
+++ src/sounds.ts
@@ -1,19 +1,19 @@
 // @ts-ignore
-import tabSwitchDefaultAudioUrl from 'url:./assets/sounds/default/tabSwitch.wav';
+import tabSwitchDefaultAudioUrl from 'url:./assets/sounds/default/tabSwitch.mp3';
 // @ts-ignore
-import tabUpdatedDefaultAudioUrl from 'url:./assets/sounds/default/tabUpdate.wav';
+import tabUpdatedDefaultAudioUrl from 'url:./assets/sounds/default/tabUpdate.mp3';
 // @ts-ignore
-import tabCreatedDefaultAudioUrl from 'url:./assets/sounds/default/tabNew.wav';
+import tabCreatedDefaultAudioUrl from 'url:./assets/sounds/default/tabNew.mp3';
 // @ts-ignore
-import tabRemovedDefaultAudioUrl from 'url:./assets/sounds/default/tabClose.wav';
+import tabRemovedDefaultAudioUrl from 'url:./assets/sounds/default/tabClose.mp3';
 // @ts-ignore
-import tabSwitchNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabSwitch.wav';
+import tabSwitchNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabSwitch.mp3';
 // @ts-ignore
-import tabUpdatedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabUpdate.wav';
+import tabUpdatedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabUpdate.mp3';
 // @ts-ignore
-import tabCreatedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabNew.wav';
+import tabCreatedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabNew.mp3';
 // @ts-ignore
-import tabRemovedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabClose.wav';
+import tabRemovedNostromoAudioUrl from 'url:./assets/sounds/nostromo/tabClose.mp3';
 
 export const DefaultAudioUrls = [
   tabSwitchDefaultAudioUrl,
